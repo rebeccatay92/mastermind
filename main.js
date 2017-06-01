139a3f1355a6c32994ca06d1223b45919addcdff
rebeccatay92 <rebeccatay92@gmail.com>
2017-06-01 12:34:16 +0800

    Basic layout and color paletter

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log("Javascript is running");
+
+//generate random 4 digit code with numbers 0 to 5.
+secretCode = [];
+for (i=0; i < 4; i ++) {
+	secretCode.push(Math.floor(Math.random()*6));
+}
+console.log(secretCode);
+
+//first create the color palette picker
+//use numbers 0 to 5 for colors.
+
+//when clicking on another color, this resets the border around previous selections
+paletteCircle = document.getElementsByClassName("paletteCircle");
+paletteCircleReset = function() {
+	for (i=0; i<paletteCircle.length; i++) {
+		paletteCircle[i].style.border="1px solid black";
+	}
+}
+
+//creates event listeners for each palette circle
+//resets the css, adds border and changes currentColor variable
+for (i=0; i<paletteCircle.length; i ++) {
+	paletteCircle[i].addEventListener("click", function() {
+		paletteCircleReset();
+		this.style.border="4px solid black";
+		currentColor = i;
+	})
+}
+
